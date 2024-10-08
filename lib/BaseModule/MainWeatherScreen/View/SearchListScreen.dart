846ee2bf560d16108a9dbc1ba2b0b844536e1aec
Yousuf Raza <yousuf.raza@brainstation-23.com>
2024-10-08 20:01:27 +0600

    cache data

--- lib/BaseModule/MainWeatherScreen/View/SearchListScreen.dart
+++ lib/BaseModule/MainWeatherScreen/View/SearchListScreen.dart
@@ -1,16 +1,16 @@
-
 import 'package:flutter/material.dart';
 import 'package:weather_track/BaseModule/MainWeatherScreen/ViewModel/SearchListViewModel.dart';
+
 class SearchListScreen extends StatefulWidget {
   final ValueChanged<String> onUpdate;
-  SearchListScreen({ required this.onUpdate});
+
+  SearchListScreen({required this.onUpdate});
+
   @override
   _SearchListScreenState createState() => _SearchListScreenState();
 }
-
 class _SearchListScreenState extends State<SearchListScreen> {
   TextEditingController _searchController = TextEditingController();
-
   final viewModel = SearchListViewModel();
   List<String> _filteredItems = [];
   String? _selectedItem;
@@ -18,8 +18,15 @@ class _SearchListScreenState extends State<SearchListScreen> {
   @override
   void initState() {
     super.initState();
-    _filteredItems = viewModel.getAllCityName(); // Initially show all items
+    _initializeData(); // Separate async method
     _searchController.addListener(_filterItems);
+  }
+
+  // Separate method to handle async logic
+  Future<void> _initializeData() async {
+    _filteredItems = await viewModel.getAllCityName(); // Initially show all items
+    setState(() {}); // Update UI after fetching data
+    await viewModel.hasConnection(); // Call hasConnection after fetching city names
   }
 
   @override
@@ -28,12 +35,21 @@ class _SearchListScreenState extends State<SearchListScreen> {
     super.dispose();
   }
 
-  void _filterItems() {
+  void _filterItems() async {
+    // Get the search query from the search controller
+    String query = _searchController.text.toLowerCase();
+
+    // Fetch the city names asynchronously from the view model
+    List<String> cityNames = await viewModel.getAllCityName();
+
+    // Filter the city names based on the query
+    List<String> filteredList = cityNames
+        .where((city) => city.toLowerCase().contains(query))
+        .toList();
+
+    // Update the filtered items and refresh the UI
     setState(() {
-      String query = _searchController.text.toLowerCase();
-      _filteredItems = viewModel.getAllCityName()
-          .where((item) => item.toLowerCase().contains(query))
-          .toList();
+      _filteredItems = filteredList;
     });
   }
 
@@ -46,8 +62,7 @@ class _SearchListScreenState extends State<SearchListScreen> {
             onPressed: () {
               Navigator.pop(context);
             },
-            icon: Icon(Icons.arrow_back)
-        ),
+            icon: Icon(Icons.arrow_back)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -63,11 +78,13 @@ class _SearchListScreenState extends State<SearchListScreen> {
             ),
             SizedBox(height: 16),
             _selectedItem != null
-                ? Text('Selected: $_selectedItem',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ))
+                ? Text(
+              'Selected: $_selectedItem',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            )
                 : Container(),
             SizedBox(height: 16),
             Expanded(
--- lib/BaseModule/MainWeatherScreen/ViewModel/WeatherScreenViewModel.dart
+++ lib/BaseModule/MainWeatherScreen/ViewModel/WeatherScreenViewModel.dart
@@ -1,48 +1,87 @@
-
-
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../Model/WeatherModel.dart';
 import 'package:intl/intl.dart';
 import '../Model/CityWeatherModel.dart';
+import 'dart:convert'; // For jsonEncode and jsonDecode
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 class WeatherViewModel {
+  Future<bool> hasConnection() async {
+    return await InternetConnection().hasInternetAccess;
+  }
 
   Future<WeatherDetails?> fetchWeather(String cityName) async {
-    const apiKey = '0e467e5c6beceddf2be0be537b297025'; // Replace with your API key
+    const apiKey =
+        '0e467e5c6beceddf2be0be537b297025'; // Replace with your API key
     final url = Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric');
 
-    final response = await http.get(url);
+    final isConnected = await hasConnection();
+    print(isConnected);
+    if (isConnected == true) {
+      final response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      // Decoding the response
-      return  WeatherDetails.fromRawJson(response.body);
+      if (response.statusCode == 200) {
+        String jsonString = jsonEncode(response.body);
+        addWeatherByCityName(jsonString, cityName);
 
+
+        saveListOfSearchedCityName(cityName);
+
+        return WeatherDetails.fromRawJson(response.body);
+      } else {
+        throw Exception(
+            'Failed to load weather data: ${response.statusCode} ${response.reasonPhrase}');
+      }
     } else {
-      throw Exception('Failed to load weather data: ${response.statusCode} ${response.reasonPhrase}');
+      String? weather = await getWeatherFromCityName(cityName);
+      print('weather form local data = $weather');
+
+      if (weather != null) {
+        final json = jsonDecode(weather);
+        return WeatherDetails.fromRawJson(json);
+      } else {
+        throw Exception('Failed to load weather data');
+      }
     }
   }
 
   Future<HourlyWelcome?> fetchWeatherForecast(String city) async {
     final apiKey = 'dc5faa7ba47bb772d5f0245345af9624';
-    final url = Uri.parse('https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$apiKey');
+    final url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$apiKey');
 
-    try {
-      final response = await http.get(url);
+    final isConnected = await hasConnection();
+    print(isConnected);
+    if (isConnected == true) {
+      try {
+        final response = await http.get(url);
 
-      if (response.statusCode == 200) {
-        // Successfully received response, now parse the JSON
-        print(HourlyWelcome.fromRawJson(response.body).list);
-        return HourlyWelcome.fromRawJson(response.body);
-      } else {
-        // Handle error if the request fails
-        print('Error: ${response.statusCode}');
+        if (response.statusCode == 200) {
+          String jsonString = jsonEncode(response.body);
+          addWeatherByCityName(jsonString, '${city}Hourly');
+
+          return HourlyWelcome.fromRawJson(response.body);
+        } else {
+          // Handle error if the request fails
+          print('Error: ${response.statusCode}');
+          return null;
+        }
+      } catch (e) {
+        print('Error: $e');
         return null;
       }
-    } catch (e) {
-      print('Error: $e');
-      return null;
+    } else {
+      String? weather = await getWeatherFromCityName('${city}Hourly');
+      print('weather form local data = $weather');
+
+      if (weather != null) {
+        final json = jsonDecode(weather);
+        return HourlyWelcome.fromRawJson(json);
+      } else {
+        throw Exception('Failed to load weather data');
+      }
     }
   }
 
@@ -65,6 +104,41 @@ class WeatherViewModel {
     double celsius = kelvin - 273.15;
 
     // Return the Celsius value as an integer
-    return celsius.round();  // Use .round() to get the nearest integer
+    return celsius.round(); // Use .round() to get the nearest integer
+  }
+
+  addWeatherByCityName(String jsonString, String cityName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setString(cityName, jsonString);
+  }
+
+  Future<String?> getWeatherFromCityName(String cityName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? jsonString = prefs.getString(cityName);
+    print('successfully get');
+    return jsonString;
+  }
+
+  saveListOfSearchedCityName(String cityName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? listOfCity = prefs.getStringList('SavedCityList');
+    if (listOfCity == null) {
+      List<String> savedCityName = [cityName];
+      await prefs.setStringList('SavedCityList', savedCityName);
+    } else {
+      List<String> savedCityName = listOfCity!;
+      savedCityName.add(cityName);
+      Set<String> setOfCity = savedCityName.toSet();
+      savedCityName = setOfCity.toList();
+
+      await prefs.setStringList('SavedCityList', savedCityName);
+    }
+  }
+
+  removeAllDataForKey(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('SavedCityList');
   }
 }
